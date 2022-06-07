1276f8cb41662356e6f25dae35f530eb7aba3414
nancy <smithnancy@outlook.com>
2022-06-07 12:54:54 +1000

    12-Stu-nancy Conditional Statements

--- 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,14 @@
 // WRITE YOUR CODE HERE
+/** set the array students */
+var students = ['Angy','karol','Cesar'];
+/**  welcome  each student */
+console.log("tota; students: "+students.length);
+console.log("Welcome to the class: "+students[0]);
+console.log("Welcome to the class: "+students[1]);
+console.log("Welcome to the class: "+students[2]);
+/** replace the name of the first student */
+students[0]='Carlos';
+/** verify  the name  has been replaced */
+if(students[0]=='Carlos'){
+    console.log(students[0]+" is in class");
+}
